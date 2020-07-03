7c1b2d7e39f3103b67a2e61ef142dc907fce34fa
Hyung Kim <hjkmines@gmail.com>
2020-07-03 19:20:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ attr_reader :id
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
 end
