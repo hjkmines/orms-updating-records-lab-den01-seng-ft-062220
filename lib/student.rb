fa2c149cc16918b21c1d8b6a5ce6a85ebe771a22
Hyung Kim <hjkmines@gmail.com>
2020-07-03 19:33:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,10 +38,9 @@ attr_reader :id
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
   
-  def self.create
+  def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save 
-    student
   end 
   
   def self.new_from_db(row)
